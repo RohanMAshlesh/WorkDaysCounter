a5ccec7741bb97e930c9ff83b1f40ba1f17732a4
Rohan Manjunath Ashlesh <rashlesh@deloitte.com>
2022-01-27 17:52:56 +0530

    main functionality

--- app/src/main/java/com/ashlesh/workdayscounter/MainActivity.kt
+++ app/src/main/java/com/ashlesh/workdayscounter/MainActivity.kt
@@ -2,10 +2,29 @@ package com.ashlesh.workdayscounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.ashlesh.workdayscounter.databinding.ActivityMainBinding
+import org.joda.time.DateTime
+import org.joda.time.Days
+import org.joda.time.format.DateTimeFormat
+import java.text.SimpleDateFormat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        calculateDays()
+    }
+
+    fun calculateDays() {
+        val dateString: String = "14/09/2020"
+        val sdf: SimpleDateFormat = SimpleDateFormat(dateString)
+        val dateTime: DateTime = DateTime.parse("14/09/2020",
+            DateTimeFormat.forPattern("dd/MM/yyyy"))
+        val currentDate = DateTime.now()
+        val dayCount = Days.daysBetween(dateTime,currentDate).days
+        binding.daysCount.text = dayCount.toString()
     }
 }
